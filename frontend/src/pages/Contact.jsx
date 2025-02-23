a6b2c1d87b320b419a206c3c9bce318082aeed38
Akshay kumar <akshaykumar880466@gmail.com>
2025-02-23 19:35:58 +0530

    init

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,8 +1,23 @@
+import { assets } from "../assets/assets"
 
 const Contact = () => {
   return (
     <div>
-      
+      <div className="text-center text-2xl pt-10 text-gray-500">
+        <p>Contact <span>US</span></p>
+      </div>
+      <div className="my-10 flex flex-col md:flex-row justify-center gap-10 mb-20 ">
+        <img className="w-[90%] md:w-[360px]" src={assets.contact_image} alt="" />
+        <div className="flex flex-col gap-4 justify-center items-start ">
+          <p className="font-semibold text-lg text-gray-600 ">OUR OFFICE</p>
+
+          <p className="text-gray-500">Prestio Hospital </p> <br /> near laal mandir , Jehanabad , Bihar , India 
+          <p className="text-gray-500">Tel : 9847038294 <br />Email : akshaydev@gmail.com</p>
+          <p className="font-semibold text-lg text-gray-500">Careers at our hospital </p>
+          <p className="text-gray-500">Learn more about our team and job openings.</p>
+          <button className="border border-black px-8 py-4 text-sm hover:text-white hover:bg-primary transition-all duration-300">Explore job</button>
+        </div>
+      </div>
     </div>
   )
 }
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,89 @@
+import { useState } from "react";
 
 const Login = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [state, setState] = useState("Sign Up");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
 
-export default Login
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form className="min-h-[80vh] flex items-center">
+      <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 shadow-lg ">
+        <p className="text-2xl font-semibold">
+          {state === "Sign Up" ? "Create Account" : "Login"}
+        </p>
+        <p>
+          Please {state === "Sign Up" ? "Create Account" : "Login"} to book
+          Appointemt
+        </p>
+
+        {state === "Sign Up" && (
+          <div className="w-full">
+            <p>Full Name</p>
+            <input
+              className="border border-zinc-300 rounded w-full p-2 mt-1"
+              type="text"
+              placeholder="enter your name"
+              onChange={(e) => setName(e.target.name)}
+              value={name}
+            />
+          </div>
+        )}
+
+        <div className="w-full">
+          <p>Email </p>
+          <input
+            className="border border-zinc-300 rounded w-full p-2 mt-1"
+            type="text"
+            placeholder="enter your email"
+            onChange={(e) => setEmail(e.target.email)}
+            value={email}
+          />
+        </div>
+
+        <div className="w-full">
+          <p>Password</p>
+          <input
+            className="border border-zinc-300 rounded w-full p-2 mt-1"
+            type="text"
+            placeholder="enter your password"
+            onChange={(e) => setPassword(e.target.password)}
+            value={password}
+          />
+        </div>
+
+        <button className="bg-primary text-white w-full py-2 rounded-md text-base">
+          {state === "Sign Up" ? "Create Account" : "Login"}
+        </button>
+
+        {state === "Sign Up" ? (
+          <p>
+            Already have an account ?{" "}
+            <span
+              onClick={() => setState("Login")}
+              className="text-primay underline cursor-pointer"
+            >
+              Login here
+            </span>{" "}
+          </p>
+        ) : (
+          <p>
+            Create an new account ?{" "}
+            <span
+              onClick={() => setState("Sign Up")}
+              className="text-primay underline cursor-pointer"
+            >
+              click here
+            </span>{" "}
+          </p>
+        )}
+      </div>
+    </form>
+  );
+};
+
+export default Login;
