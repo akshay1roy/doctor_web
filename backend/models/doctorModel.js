e378f12bd952190ada69035d49bbc035acc916bf
Akshay kumar <akshaykumar880466@gmail.com>
2025-03-04 11:47:59 +0530

    init

---
+++ backend/models/doctorModel.js
@@ -0,0 +1,62 @@
+
+import mongoose from "mongoose";
+const doctorSchema=mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+
+    },
+    image:{
+        type:String,
+        requied:true
+    },
+    speciality:{
+        type:String,
+        required:true,
+    },
+    degree:{
+        type:String,
+        required:true
+    },
+    experiece:{
+        type:String,
+        required:true
+    },
+    about:{
+        type:String,
+        required:true
+    },
+    available:{
+        type:Boolean,
+        required:true
+    },
+    frees:{
+        type:Number,
+        required:true
+    },
+    address:{
+        type:Object,
+        required:true
+    },
+    date:{
+        type:Number,
+        required:true,
+    },
+    slots_booked:{
+        type: Object,
+        default:{}
+    }
+},{minimize:false})
+
+const doctorModel=mongoose.models.doctor || mongoose.model("doctor",doctorSchema);
+
+export default doctorModel;
