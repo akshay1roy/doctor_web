f72f4383a1f0ac2456158e491ff9987ae946c89b
Akshay kumar <akshaykumar880466@gmail.com>
2025-03-06 22:40:57 +0530

    admin init

---
+++ admin/src/App.jsx
@@ -0,0 +1,14 @@
+
+// import React from 'react'
+
+import { Login } from "./pages/Login"
+
+const App = () => {
+  return (
+    <div className="bg-yellow-500" >
+      <Login/>
+    </div>
+  )
+}
+
+export default App
